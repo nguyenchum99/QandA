55fb8e26437395af5d33778729e76bf75c6f7dca
= <=>
2019-10-27 16:40:26 +0700

    update in table search_User

--- resources/views/admin/user/search_user.blade.php
+++ resources/views/admin/user/search_user.blade.php
@@ -1,16 +1,19 @@
 @extends('admin.layouts.index')
 @section('content')
     
-    <table class="table table-striped">
+    <table border="2" class="table table-striped">
         <h4>Tìm kiếm: {{$tukhoa}}</h4>
-        <!-- <tr id="tbl-first-row">
-            <td width="5%">id</td>
+        <tr id="tbl-first-row" style="font-weight: bold;">
+            <td width="5%">Id</td>
             <td width="30%" style="white-space: nowrap ;">Tên đăng nhập</td>
             <td width="30%">E-mail</td>
             <td width="20%">Mật khẩu</td>
+            <td width="20%">Avatar</td>
             <td width="5%">Level</td>
+            <td width="5%">Sửa</td>
+            <td width="5%">Xóa</td>
         </tr>
- -->
+
         
         @foreach($user as $u)
             <tr>
@@ -18,6 +21,7 @@
                 <td>{{$u->name}}</td>
                 <td>{{$u->email}}</td>
                 <td>{{$u->password}}</td>
+                <td>{{$u->avatar}}</td>
                 <td>
                         @if($u->level == 1)
                             {{"Admin"}}
